826042f359d079ee5e9f6565bbf7e31f364d61bf
Nakorn Thanhai <Nakorn.thanhai@gmail.com>
2020-11-16 14:06:11 +0700

    mpmsc2

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI : "mongodb+srv://admin:12345678a@shoppper.osgku.mongodb.net/test" //mongodb+srv://admin:12345678a@shoppper.osgku.mongodb.net/test
+    mongoURI : "mongodb+srv://admin:31081993@shoppper.osgku.mongodb.net/test" //mongodb+srv://admin:12345678a@shoppper.osgku.mongodb.net/test
 }
